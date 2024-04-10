3a49e4ff7b121a9e5dbe9ef2552e1e64e3dd0e23
apsaires <andre.p.aires@gmail.com>
2024-04-10 09:50:12 +0100

    delete

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -20,6 +20,8 @@ public class SellerDaoJDBC implements SellerDao{
 	
 	private Connection conn;
 	
+	
+	
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
@@ -92,8 +94,20 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+
+			st.setInt(1, id);
+
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
